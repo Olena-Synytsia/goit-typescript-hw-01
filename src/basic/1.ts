531d7113422815ddd7240d1e53a59f697410d2db
Olena-Synytsia <alenasinitsa85@gmail.com>
2024-10-22 23:26:27 +0300

    1.ts

--- src/basic/1.ts
+++ src/basic/1.ts
@@ -1,7 +1,7 @@
-// const age = 50;
-// const username = "Max";
-// const toggle = true;
-// const empty = null;
-// const callback = (a) => {
-//   return 100 + a;
-// };
+let age: number = 50;
+let username: string = "Max";
+let toggle: boolean = true;
+const empty = null;
+const callback: (a: number) => number = (a) => {
+  return 100 + a;
+};
